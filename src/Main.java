c870f23cf9db30d9f6e15354da231572252b73c7
dublin <daydu@mail.uc.edu>
2025-09-29 19:24:44 -0400

    Lab 5 assignment

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String [] agrs) {
+public class Main {//Class partyAffiliation
+    public static void main(String [] agrs) {//main()
         Scanner in = new Scanner(System.in);
-        String politicParty = "";
-        System.out.println("Enter your political party [D, R, I, or Other}");
-        politicParty = in.nextLine();
-        if (politicParty.equalsIgnoreCase("D")) {
-            System.out.println("You get a Democratic Donkey!");
-        }
-        else if (politicParty.equalsIgnoreCase("R")) {
-            System.out.println("You get a Republican Elephant!");
-        }
-        else if (politicParty.equalsIgnoreCase("I")) {
-            System.out.println("You get a Independent Person!");
-        }
-        else
+        String politicParty = "";//userParty = 0
+        System.out.println("Enter your political party [D, R, I, or Other}");//output "Enter your political party using R, D, or I"
+        politicParty = in.nextLine();//input userParty
+        if (politicParty.equalsIgnoreCase("D")) {//if userParty == democrat
+            System.out.println("You get a Democratic Donkey!");//output "You get a Democratic Donkey!"
+        } else if (politicParty.equalsIgnoreCase("R")) {//else if userParty == republican
+            System.out.println("You get a Republican Elephant!");//output "You get a Republican Elephant"
+        } else if (politicParty.equalsIgnoreCase("I")) {//else if userParty==Independent
+            System.out.println("You get a Independent Person!");//output "You get a Independent Man"
+        } else//else
             System.out.println("You are apart of another party!");
-    }
-}
+        {
+            ;//output "You are apart of another party!"
+        }//endIf
+    }//return
+}//endClass
